767d8ec3a56e7a9189b6e3bee1e972975618c54c
neonael.com <neonael.angcaya.nazareno@gmail.com>
2025-02-22 16:39:24 +0800

    Initial Commit

---
+++ src/main/java/com/rocs/practical/exam/application/app/facade/ecllipse/Impl/EllipseFacadeImpl.java
@@ -0,0 +1,18 @@
+package com.rocs.practical.exam.application.app.facade.ecllipse.Impl;
+
+import com.rocs.practical.exam.application.app.facade.ecllipse.Ellipse;
+import com.rocs.practical.exam.application.model.circle.Circle;
+
+public class EllipseFacadeImpl implements Ellipse {
+
+    Circle getCircle =  new Circle();
+    @Override
+    public Circle getArea() {
+        return null;
+    }
+
+    @Override
+    public Circle getCircumference() {
+        return null;
+    }
+}

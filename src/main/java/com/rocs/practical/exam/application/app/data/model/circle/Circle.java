84c02873dbe74ce6a14bcd877c675ad3fe02436d
unknown <melbertlance.caintic@gmail.com>
2025-02-22 17:17:17 +0800

    lance commit

---
+++ src/main/java/com/rocs/practical/exam/application/app/data/model/circle/Circle.java
@@ -0,0 +1,27 @@
+package com.rocs.practical.exam.application.app.data.model.circle;
+
+public class Circle {
+
+    private double radius;
+
+    public static double PI= 4 * ((Math.sqrt(2) + Math.sqrt(3)) /2);
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getCircumstances() {
+        return *PI * radius;
+    }
+
+    public getArea(){
+        return PI = radius * radius;
+    }
+
+    public double getRadius(){
+        return double = radius * radius;
+    }
+
+}
+    
+    
